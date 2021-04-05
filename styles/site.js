f35fd38cce7bf7fa4a331b3bd2d0f8d2e4cadbf9
Tobias Reich <tobias.reich.ich@gmail.com>
2021-04-05 18:32:49 +0200

    Adjust site variables

--- styles/site.js
+++ styles/site.js
@@ -1,20 +1,18 @@
 export default `
 	html {
 		/* Colors */
-		--background: hsla(110, 6%, 13%, 1);
-		--before: hsla(0, 0%, 100%, .05);
-		--behind: hsla(0, 0%, 0%, .35);
+		--background: hsla(45, 36%, 96%, 1);
 		--primary: hsla(22, 100%, 50%, 1);
-		--destructive: hsla(15, 100%, 50%, 1);
-		--destructive-background: hsla(15, 100%, 20%, .6);
-		--header: hsla(0, 0%, 100%, 1);
-		--text: hsla(0, 0%, 100%, .8);
+		--header: hsla(0, 0%, 0%, 1);
+		--text: hsla(0, 0%, 0%, .7);
 
 		/* Sizes */
-		--font-size: 140%;
+		--font-size: 150%;
 
 		/* Font Families */
-		--font-primary: 'Helvetica Neue', sans-serif;
+		--font-header: 'Staatliches', sans-serif;
+		--font-text: 'Source Serif Pro', serif;
+		--font-ui: 'Source Sans Pro', sans-serif;
 	}
 
 	body {
