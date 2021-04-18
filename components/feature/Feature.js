f7016cc74ff72f933f10004e80588a20744bd80f
Tobias Reich <tobias.reich.ich@gmail.com>
2021-04-18 12:46:13 +0200

    Feature title font and feature spacing

--- components/feature/Feature.js
+++ components/feature/Feature.js
@@ -4,9 +4,7 @@ import style from './feature.module.scss'
 export default function Feature(props) {
 	return (
 		<div className={ style.container }>
-			<div className={ style.icon }>
-				{ props.icon }
-			</div>
+			{ props.icon }
 			<h2 className={ style.title }>{ props.title }</h2>
 			<p>{ props.children }</p>
 		</div>
